fa604ead83521882d7967f4c6cd815c52693c53f
gadi <gadihylmi66@gmail.com>
2026-09-16 10:50:14 +0700

    Update frontend perpustakaan2

--- frontend-perpus/src/utils/api.js
+++ frontend-perpus/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: 'http://10.39.148.41:8000/api',
   headers: {
     Accept: 'application/json',
   },
